9e67f3356eb3606ac1f858c711b54de13c205353
Daniel Coombs <danielcoombs005@yahoo.com>
2019-06-12 13:56:47 -0500

    Change Library to Domain

---
+++ Project2Solution/Project.Test/NUnit.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Project.Test
+{
+    class Class1
+    {
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        [Test]
+        public void TestMethod()
+        {
+
+        }
+    }
+}
