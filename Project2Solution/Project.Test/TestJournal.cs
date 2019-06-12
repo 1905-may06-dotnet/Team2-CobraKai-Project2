e35c406167f7528881d0069cd9ea9872ecf05d04
Daniel Coombs <danielcoombs005@yahoo.com>
2019-06-12 12:42:19 -0500

    add MVC, remove Model

---
+++ Project2Solution/Project.Test/TestJournal.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Project.Test
+{
+    class TestJournal
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Create()
+        {
+
+        }
+
+        [Test]
+        public void Read()
+        {
+
+        }
+
+        [Test]
+        public void Delete()
+        {
+
+        }
+    }
+}
---
+++ Project2Solution/Project.Test/TestPlaylist.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Project.Test
+{
+    class TestPlaylist
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Create()
+        {
+
+        }
+
+        [Test]
+        public void Read()
+        {
+
+        }
+
+        [Test]
+        public void Delete()
+        {
+
+        }
+    }
+}
