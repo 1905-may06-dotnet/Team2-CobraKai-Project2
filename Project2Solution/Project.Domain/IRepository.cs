fd8f8f902dfd2a9548d66a9bfecadc03a9a31de6
Daniel Coombs <danielcoombs005@yahoo.com>
2019-06-15 02:08:16 -0500

    updated Repository and IRepository

--- Project2Solution/Project.Domain/IRepository.cs
+++ Project2Solution/Project.Domain/IRepository.cs
@@ -9,40 +9,29 @@ namespace Project.Domain
         IEnumerable<Journal> GetJournals();
         Journal GetJournalById(int id);
         Journal GetJournalByTitle(string title);
-        int AddJournal(); //returns 1 if successful, 0 if not
-        Journal UpdateJournal(Journal journal);
+        void CreateJournal(Journal journal); //returns 1 if successful, 0 if not
+        void UpdateJournal(Journal journal);
         void DeleteJournal(int id);
-
-
-        //IEnumerable<ListEntry> GetListEntries();
-
-
-        //IEnumerable<MusicList> GetMusicLists();
-
-
-        IEnumerable<Person> GetPersons();
-
-
-        //IEnumerable<PlayList> GetPlayLists();
-
 
         IEnumerable<Song> GetSongs();
         Song GetSongById(int id);
         Song GetSongByTitle(string title);
-        int CreateSong(); //return 1 if successful
-        Song UpdateSong(Song song);
+        void CreateSong(Song song); //return 1 if successful
+        void UpdateSong(Song song);
         void DeleteSong(int id);
 
-        Person CreatePerson();
-        //Person ReadPerson();
-        Person UpdatePerson();
-        Person DeletePerson();
+        IEnumerable<Person> GetPersons();
+        Person GetPersonById(int id);
+        void CreatePerson(Person person); //returns 1 if successful
+        void UpdatePerson(Person person);
+        void DeletePerson(int id);
 
-/*        PlayList CreatePlayList();
-        //PlayList ReadPlayList();
-        PlayList UpdatePlayList();
-        PlayList DeletePlayList();
-*/
+        IEnumerable<PlayList> GetPlayLists();
+        PlayList GetPlayListById(int id);
+        PlayList GetPlayListByTitle(string title);
+        void CreatePlayList(PlayList playlist);
+        void UpdatePlayList(PlayList playlist);
+        void DeletePlayList(int id);
 
 
         int Save(); //returns 1 if successful, 0 if not
