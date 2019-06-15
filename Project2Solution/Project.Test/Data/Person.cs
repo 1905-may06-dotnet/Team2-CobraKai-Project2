682ed0c243ddaec71ebc2de1c9ca6e4cebbeadd6
Daniel Coombs <danielcoombs005@yahoo.com>
2019-06-15 13:29:16 -0500

    Added Create Test for Person

---
+++ Project2Solution/Project.Test/Data/Person.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Project.Data;
+
+namespace Project.Test
+{
+    public class Person
+    {
+        static Project.Domain.Person unitTest = new Project.Domain.Person();
+        Project.Data.Entities.CobraKaiDbContext db = new Project.Data.Entities.CobraKaiDbContext();
+
+        [Test]
+        public void A_Create()
+        {
+            Project.Data.Repository a = new Project.Data.Repository(db);
+            unitTest = new Project.Domain.Person();
+            a.CreatePerson(unitTest);
+            Assert.Pass();
+        }
+
+        //[Test]
+        //public void D_Delete()
+        //{
+
+        //}
+    }
+}
