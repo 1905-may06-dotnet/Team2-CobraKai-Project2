93b594a74b5d1f43b2bec92dd2d8c944d05e6dbc
Daniel Coombs <danielcoombs005@yahoo.com>
2019-06-25 15:17:25 -0500

    Made changes to allow to publish

--- Project2Solution/Music-Journal/src/app/playlist/playlist.component.ts
+++ Project2Solution/Music-Journal/src/app/playlist/playlist.component.ts
@@ -11,7 +11,7 @@ import * as $ from 'jquery';
 
 export class PlaylistComponent implements OnInit, AfterViewInit {
 
-  show : string = "";
+  show : boolean = false;
 
   ngAfterViewInit(): void {
 
