5aa36cbbdef203dae2628305a3e0a8bd0f9a37a7
FredBrume <FredBrume@users.github.com>
2019-06-24 21:45:25 -0500

    latest commit

--- Project2Solution/Project.Client/Startup.cs
+++ Project2Solution/Project.Client/Startup.cs
@@ -14,57 +14,57 @@
 using Microsoft.Net.Http.Headers;
 
 namespace Project.Client
+{
+    public class Startup
     {
-        public class Startup
+        public Startup(IConfiguration configuration)
         {
-            public Startup(IConfiguration configuration)
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(options =>
             {
-                Configuration = configuration;
+                options.AddPolicy(MyAllowSpecificOrigins,
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowAnyOrigin()
+                    .WithHeaders("Accept", "Content-Type", "Origin", "X-My-Header")
+                    .WithExposedHeaders("Content-Disposition", "Content-Length"));
+
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<Project.Data.Entities.CobraKaiDbContext>(optionsAction =>
+            optionsAction.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<Project.Domain.IRepository, Project.Data.Repository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseMaintainCorsHeaders();
             }
-
-		// This method gets called by the runtime. Use this method to add services to the container.
-		readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-		public IConfiguration Configuration { get; }
-
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-
-			services.AddCors(options =>
-			{
-				options.AddPolicy(MyAllowSpecificOrigins,
-					builder => builder.AllowAnyOrigin()
-					.AllowAnyMethod()
-					.AllowAnyHeader()
-					.AllowAnyOrigin()
-					.WithHeaders("Accept", "Content-Type", "Origin", "X-My-Header")
-					.WithExposedHeaders("Content-Disposition", "Content-Length"));
-
-			});
-
-
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-			services.AddDbContext<Project.Data.Entities.CobraKaiDbContext>(optionsAction =>
-			optionsAction.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-			services.AddScoped<Project.Domain.IRepository, Project.Data.Repository>();
-		}
-
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            else
             {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
-                    //app.UseMaintainCorsHeaders();
-                }
-                else
-                {
-                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                    app.UseHsts();
-                }
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.UseCors(MyAllowSpecificOrigins);
 
@@ -72,3 +72,4 @@ namespace Project.Client
             app.UseMvc();
         }
     }
+}
